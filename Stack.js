28eb492be32f2dfaef37437a8a8488b6d52ca73c
pradip <pradip.chotara@aeonx.digital>
2024-05-02 10:27:54 +0530

    added stack.js

---
+++ Stack.js
@@ -0,0 +1,42 @@
+// stack.mjs
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            throw new Error('Stack is empty');
+        }
+        return this.items.pop();
+    }
+
+    peek() {
+        if (this.isEmpty()) {
+            throw new Error('Stack is empty');
+        }
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
+    }
+
+    print() {
+        console.log(this.items.toString());
+    }
+}
+
+export default Stack;
